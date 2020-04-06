c6614d317506e8cd5e644eafa5ef214e71546da5
nicolasz-cri <nicolas.zilbermann@cri-paris.org>
2020-04-06 10:59:43 +0200

    feat: Add VueJS example Integration

---
+++ examples/vuejs-map/src/components/WeLearn.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <h1>Test Welearn Map</h1>
+        <div id="atlas"></div>
+    </div>
+</template>
+
+<script>
+import { setupMapView } from '../../../../src/renderer'
+import '../assets/dotatlas'
+
+export default {
+    name: 'Welearn',
+    methods: {
+        displayData: function(e) {
+            e.detail.selection.forEach(element => {
+                console.log(element)
+            });
+        }
+    },
+    mounted() {
+        window.addEventListener('searchMap', this.displayData);
+        const atlasEl = document.getElementById('atlas');
+        setupMapView({element: atlasEl});
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: white;
+    }
+    #atlas {
+        width: 1000px;
+        height: 400px;
+    }
+</style>
