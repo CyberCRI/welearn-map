8461f3cbb919828c9425ba618ec2fcba9ddc07a7
Prashant Sinha <prashant+git@noop.pw>
2020-04-09 18:45:46 +0200

    clean up layers

--- src/layers.js
+++ src/layers.js
@@ -1,5 +1,6 @@
 import { MapLayerAPI } from '@ilearn/modules/api'
 
+
 const trimLabel = (label) => {
   // If the label has >= 6 words, we'd add '...'.
   // Split the label text on space characters (\s)
@@ -22,21 +23,25 @@ const takeValues = (concept, lang) => {
   }
 }
 
-export const fetchBaseLayer = async () => {
-  return await MapLayerAPI.everything()
-    .then((nodes) =>
-      nodes.map(p => {
-        return {
-          ...p,
-          x: p.x_map_en,
-          y: p.y_map_en,
-          userData: true,
-          ...(takeValues(p, 'en') || takeValues(p, 'fr')),
-          elevation: .8,
-          markerShape: 'circle',
-          markerSize: 4,
-          labelOpacity: 1,
-          labelPriority: (p.n_items || 1),
-        }
-      }).filter(p => p.x && p.y))
+const nodeMapper = p => {
+  ...p,
+  x: p.x_map_en,
+  y: p.y_map_en,
+  userData: true,
+  ...(takeValues(p, 'en') || takeValues(p, 'fr')),
+  elevation: .8,
+  markerShape: 'circle',
+  markerSize: 4,
+  labelOpacity: 1,
+  labelPriority: (p.n_items || 1),
 }
+
+const nodeFilter = p => p.x && p.y
+
+export const fetchBaseLayer = async () =>
+  fetch(`${env.api_root}/api/resources/map/base`, {
+    method: 'get',
+    mode: 'cors',
+    headers: { 'Content-Type': 'application/json' },
+  }).then(r => r.json())
+    .then(nodes => nodes.map(nodeMapper).filter(nodeFilter))
